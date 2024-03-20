efc53c5610020e9e8031dec36a278d519525474a
wwitsken <witsken.w@gmail.com>
2024-03-20 09:06:57 -0700

    Refactor getVariables function to handle exceptions

--- src/utils/getVariables.ts
+++ src/utils/getVariables.ts
@@ -6,17 +6,21 @@ export interface Variable {
 }
 
 export const getVariables = () => {
-  const customVariables = app.activeDocument.textVariables
-    .everyItem()
-    .properties.filter((variable) => variable.variableOptions?.constructorName === 'CustomTextVariablePreference');
+  try {
+    const customVariables = app.activeDocument.textVariables
+      .everyItem()
+      .properties.filter((variable) => variable.variableOptions?.constructorName === 'CustomTextVariablePreference');
 
-  const initialVariables: Variable[] = [];
+    const initialVariables: Variable[] = [];
 
-  customVariables.forEach((variable) => {
-    initialVariables.push({
-      name: variable.name,
-      contents: variable.variableOptions.properties.contents,
+    customVariables.forEach((variable) => {
+      initialVariables.push({
+        name: variable.name,
+        contents: variable.variableOptions.properties.contents,
+      });
     });
-  });
-  return initialVariables;
+    return initialVariables;
+  } catch (e) {
+    return [];
+  }
 };
